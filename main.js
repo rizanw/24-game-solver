5790eac7057d1f6042a3920090996afe2ec90110
rizanw <rzkandre@gmail.com>
2021-08-07 19:59:55 +0700

    deploy heroku

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ var favicon = require("serve-favicon");
 const { solve24Game } = require("./solver");
 
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 3000;
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.set("views", path.join(__dirname, "views"));
